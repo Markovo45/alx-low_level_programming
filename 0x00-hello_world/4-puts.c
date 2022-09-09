99fc22375532504aa8999b0a139dc6a5254c3c94
Mark <emunemu.mark@gmail.com>
2022-09-08 17:21:24 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,5 @@
-#!/bin/bash
-echo 'puts("Programming is like building a multilingual puzzle.\n")' > CFILE | gcc $CFILE
+#include<stdio.h>
+int main()
+{
+	puts("Programming is like building a multilingual puzzle,\n");
+}
