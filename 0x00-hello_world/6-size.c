e6a5b1be91119b6904566b4ecd6a41103c2377b8
Mark <emunemu.mark@gmail.com>
2022-09-08 19:05:12 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the printf function
+*
+* Description : using the main function
+* this program prints "with proper grammer, but the outcome is a piece of art
+* Return: 0
+*/
+int main(void)
+{
+	int c;
+	char a;
+	long b;
+	long long d;
+	float e;
+	double float f;
+
+	printf("%d", sizeof(c));
+	printf("%d", sizeof(a));
+	printf("%d", sizeof(b));
+	printf("%d", sizeof(d));
+	printf("%d", sizeof(e));
+	printf("%d", sizeof(f));
+	return (0);
+}
