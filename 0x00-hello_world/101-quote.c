f3dbb4b154a031503d3d447fb93ead070533a333
Mark <emunemu.mark@gmail.com>
2022-09-11 03:00:49 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+* main - print the string in the fwrite function
+*
+* Description : using the main function
+* this program prints exactly and that piece of art
+* is useful" - Dora Korpar, 2015-10-19,
+* followed by a new line, to the standard error.
+* Return: 0
+*/
+int main(void)
+{
+	writef(""and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
