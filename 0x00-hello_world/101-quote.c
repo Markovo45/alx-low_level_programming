a6c44f65085a2c748157a7ca7f0e31eac0a961e3
Mark <emunemu.mark@gmail.com>
2022-09-11 03:04:18 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -14,6 +14,6 @@
 */
 int main(void)
 {
-	writef("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (0);
 }
