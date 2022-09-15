3f0ee05336f1c794e9b87dfb4acb9655f9569f7c
Mark <emunemu.mark@gmail.com>
2022-09-14 17:29:35 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,14 +11,14 @@ int print_sign(int n)
 		return (1);
 		_putchar('+');
 	}
-	else if (n == 0)
-	{
-		return (0);
-		_putchar('0');
-	}
-	else
+	else if (n < 0)
 	{
 		return (-1);
 		_putchar('-');
+	}
+	else
+	{
+		return (0);
+		_putchar(0);
 	}
 }
