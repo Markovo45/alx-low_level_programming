5f001535a5d7169af7a85d931e4a9f6dbd42d2e0
Mark <emunemu.mark@gmail.com>
2022-09-17 15:19:05 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - computes and prints the sum of all the multiples.
+ * Description: the function main Write a program that computes
+ * and prints the sum of all the multiples of 3 or 5
+ * below 1024 (excluded)
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 0;
+	int sum = 0;
+
+	while (a < 1024)
+	{
+		if ((((a % 3) == 0) || ((a % 5) == 0)) && a != 0)
+		{
+			printf("%d, ", a);
+			sum = sum + a;
+		}
+		++a;
+	}
+	printf("%d", sum);
+	printf("\n");
+}
+
+
