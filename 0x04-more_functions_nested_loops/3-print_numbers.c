0ecf70cf416fd8ab99ce6e98e0fa0a8e47ddad79
Mark <emunemu.mark@gmail.com>
2022-09-18 15:42:35 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9.
+ * Return: 0.
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	if ((c >= '0') && (c <= '9'))
+		_putchar(c);
+	_putchar('\n');
+	return (0);
+}
+
