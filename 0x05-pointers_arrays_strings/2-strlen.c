f41ed5d4774dc083646c8138fd06f1d679531b27
Mark <emunemu.mark@gmail.com>
2022-09-19 16:23:00 -0700

    a

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swabs the value of pointer.
+ * @a: first integer argument
+ * @b: Second integer argument.
+ * Return: 0.
+ */
+int _strlen(char *s)
+{
+	int temporary = *a;
+
+	*a = *b;
+	*b = temporary;
+}
