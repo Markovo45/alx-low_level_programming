6c91766e1c59571576ccd998129185d2cc5c13d5
Mark <emunemu.mark@gmail.com>
2022-09-19 15:40:16 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - updates the value of pointer.
- * @*n: is the pointer for the function void reset_to_98.
+ * @n: is the pointer for the function void reset_to_98.
  * Return: 0.
  */
 void reset_to_98(int *n)
