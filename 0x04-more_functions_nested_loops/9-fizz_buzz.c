edd12941d3e4e3819f28f00ce538ff8f950aa436
Mark <emunemu.mark@gmail.com>
2022-09-22 13:59:34 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,23 +14,26 @@ int main(void)
 
 	while (n <= 100)
 	{
-		if ((n % 3 == 0) && (n % 5 == 0))
-		{
-			printf("FizzBuzz");
-		}
-		else if (n % 3 == 0)
+		if (n == 100)
 		{
 			printf("Fizz");
 		}
+		else if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
 		else if (n % 5 == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
 		else
 		{
 			printf("%d", n);
 		}
-		printf(" ");
 		++n;
 	}
 	printf("\n");
