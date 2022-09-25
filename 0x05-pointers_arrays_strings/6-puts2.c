8bab7ac7d4dde738ac4138546c6c849345ffc945
Mark <emunemu.mark@gmail.com>
2022-09-25 23:48:52 +0000

    A

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * _puts - prints half the length of string.
+ * _puts2 - prints half the length of string.
  * @str: the function argument
  * Return: 0.
  */
 void _puts2(char *str)
 {
-	int a = 0;
+	int a;
 	int b = 0;
 
 	while (str[b] != '\0')
 		b++;
+	a = 0;
 	while (a < b)
 	{
 		_putchar(str[a]);
