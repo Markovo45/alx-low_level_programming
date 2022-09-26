5f02e097fc17880542ff4c114a3e9ae6c20c0be4
Mark <emunemu.mark@gmail.com>
2022-09-26 23:50:04 +0000

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: String to check for needle
+ * @needle: substring to find in haystack
+ * Return: pointer to beginning of needle in haystack.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0;
+	unsigned int j;
+
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+
+		while (needle[j] && (haystack[i] == needle[0]))
+		{
+			if (haystack[i + j] == needle[j])
+				++j;
+			else
+				break;
+
+		}
+		if (needle[j])
+		{
+			++i;
+			j = 0;
+		}
+		else
+			return (haystack + i);
+	}
+	return (0);
+}
