262da03e563d27bf35baae5903997da88019cfbc
Mark <emunemu.mark@gmail.com>
2022-11-19 21:36:53 +0000

    a

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,8 +9,6 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	while (s1[i] == s2[i])
-		if (s1[i] && s2[i] != '\0')
-		++i;
+	for (i = 0; s1[i] == s2[i] && s1[i] && s2[i] != '\0'; i++)
 	return (s1[i] - s2[i]);
 }
