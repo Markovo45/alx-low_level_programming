ae41ffd139cb1d5cb12749597335b19f066ee9a7
Mark <emunemu.mark@gmail.com>
2022-11-19 17:42:53 +0000

    a

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main: _strncat - Concatenates the string pointed to by @src.
+ * @n: number of bytes of the string pointed to by @dest.
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
+ * Return: A pointer to the destination string @dest.
+ */
+char *_strncat(char *dest, char *src, int n);
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+	{
+		if (index <= n)
+		dest[dest_len++] = src[index];
+	}
+	return (dest);
+}
